455e7de55f684daae68c072e821dcb016b06ca5f
Royweru <royezi17@gmail.com>
2024-04-06 12:59:58 +0300

    modified:   components/elevate.tsx

--- components/elevate.tsx
+++ components/elevate.tsx
@@ -17,10 +17,10 @@ export const Elevate = () => {
       <h1 className=" text-3xl font-semibold font-serif my-3  text-center text-rose-400">
         HTML AND CSS
       </h1>
-      <div className=" w-full flex flex-wrap gap-4">
-        <div>
+      <div className=" w-full grid md:grid-cols-2 grid-cols-1 gap-4">
+        <div className=" col-span-1">
           <iframe
-            className=" relative md:w-[620px] sm:w-[400px] w-[450px]"
+            className=" relative w-full rounded-2xl"
             height="390"
             src="https://www.youtube.com/embed/kUMe1FH4CHE"
             title="Learn HTML – Full Tutorial for Beginners (2022)"
@@ -30,9 +30,9 @@ export const Elevate = () => {
             allowFullScreen
           ></iframe>
         </div>
-        <div>
+        <div className=" col-span-1 relative">
           <iframe
-            className=" relative md:w-[620px] sm:w-[400px] w-[450px]"
+            className=" relative w-full rounded-2xl"
             height="390"
             src="https://www.youtube.com/embed/HGTJBPNC-Gw"
             title="HTML &amp; CSS Full Course for free 🌎 (2023)"
@@ -46,10 +46,10 @@ export const Elevate = () => {
       <h1 className=" text-3xl font-semibold my-3 text-center  text-rose-400">
         JAVASCRIPT AND TYPESCRIPT
       </h1>
-      <div className=" w-full flex flex-wrap gap-4">
-        <div>
+      <div className=" w-full grid md:grid-cols-2 grid-cols-1 gap-4">
+        <div className=" col-span-1 relative">
           <iframe
-            className=" relative md:w-[620px] sm:w-[400px] w-[450px]"
+            className=" w-full relative rounded-2xl"
             height="390"
             src="https://www.youtube.com/embed/EfAl9bwzVZk"
             title="JavaScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours"
@@ -59,9 +59,9 @@ export const Elevate = () => {
             allowFullScreen
           ></iframe>
         </div>
-        <div>
+        <div className=" col-span-1 relative">
           <iframe
-            className=" relative md:w-[620px] sm:w-[400px] w-[450px]"
+            className=" w-full relative rounded-2xl"
             height="390"
             src="https://www.youtube.com/embed/gieEQFIfgYc"
             title="TypeScript Full Course for Beginners | Complete All-in-One Tutorial | 8 Hours"
@@ -71,9 +71,9 @@ export const Elevate = () => {
             allowFullScreen
           ></iframe>
         </div>
-        <div>
+        <div className=" col-span-1 relative">
           <iframe
-            className=" relative md:w-[620px] sm:w-[400px] w-[450px]"
+            className=" w-full relative rounded-2xl"
             height="390"
             src="https://www.youtube.com/embed/30LWjhZzg50"
             title="Learn TypeScript – Full Tutorial"
@@ -87,32 +87,92 @@ export const Elevate = () => {
       <h1 className=" text-3xl font-semibold my-3 text-center  text-rose-400">
         GIT AND GITHUB
       </h1>
-      <div className=" w-full flex flex-wrap gap-4">
-        <div>
-        <iframe 
-        className=" relative md:w-[620px] sm:w-[400px] w-[450px]"
-         height="390"
-          src="https://www.youtube.com/embed/hrTQipWp6co"
-           title="Git and GitHub - 0 Experience to Professional in 1 Tutorial (Part 1)"
-            frameBorder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerPolicy="strict-origin-when-cross-origin" 
-            allowFullScreen/>
-            
-        </div>
-        <div>
+      <div className=" w-full grid md:grid-cols-2 grid-cols-1 gap-4">
+        <div className=" col-span-1 relative">
           <iframe
-            className=" relative md:w-[620px] sm:w-[400px] w-[450px]"
+            className=" relative w-full  rounded-2xl"
             height="390"
-            src="https://www.youtube.com/embed/HGTJBPNC-Gw"
-            title="HTML &amp; CSS Full Course for free 🌎 (2023)"
+            src="https://www.youtube.com/embed/tRZGeaHPoaw"
+            title="Git and GitHub Tutorial for Beginners"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin"
             allowFullScreen
           />
         </div>
+        <div className="  col-span-1 relative">
+          <iframe
+            className=" relative w-full rounded-2xl"
+            height="390"
+            src="https://www.youtube.com/embed/RGOj5yH7evk"
+            title="Git and GitHub for Beginners - Crash Course"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
+      <h1 className=" text-3xl font-semibold my-3 text-center  text-rose-400">
+         THE GREAT REACT FRAMEWORK
+        </h1>
+        <div className=" w-full grid md:grid-cols-2 grid-cols-1 gap-4">
+      <div className=" col-span-1 relative">
+      <iframe 
+      className=" w-full relative rounded-2xl" 
+      height="390"
+       src="https://www.youtube.com/embed/LDB4uaJ87e0"
+        title="React Crash Course 2024" frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerPolicy="strict-origin-when-cross-origin"
+         allowFullScreen
+      />
+      </div>
+      <div className=" col-span-1 relative">
+      <iframe 
+      className=" w-full relative rounded-2xl"
+       height="390"
+        src="https://www.youtube.com/embed/CgkZ7MvWUAA"
+         title="React Full Course for free ⚛️ (2024)" 
+         frameBorder="0" 
+         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+         referrerPolicy="strict-origin-when-cross-origin" 
+         allowFullScreen
+         />
+      </div>
+        </div>
+      <h1 className=" text-3xl font-semibold my-3 text-center  text-rose-400">
+        PYTHON PROGRAMMING
+      </h1>
+      <div className=" w-full grid md:grid-cols-2 grid-cols-1 gap-4">
+        <div className=" col-span-1 relative">
+          <iframe
+            className=" w-full relative rounded-2xl"
+            height="390"
+            src="https://www.youtube.com/embed/XKHEtdqhLK8"
+            title="Python Full Course for free 🐍"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          />
+        </div>
+        <div className=" col-span-1 relative">
+        <iframe
+         className=" w-full relative rounded-2xl"
+        height="390" 
+        src="https://www.youtube.com/embed/_uQrJ0TkZlc" 
+        title="Python Tutorial - Python Full Course for Beginners" 
+        frameBorder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerPolicy="strict-origin-when-cross-origin" 
+        allowFullScreen
+        />
+        </div>
+      </div>
+      <h1 className=" text-3xl font-semibold my-3 text-center  text-rose-400">
+        
+      </h1>
     </div>
   );
 };
